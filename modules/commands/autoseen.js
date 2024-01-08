0f205d061fdedadc45836aae668aea5b8579c363
TNTxTrick <139092950+TNTxTrick@users.noreply.github.com>
2024-01-08 11:58:31 +0700

    Update autoseen.js

--- modules/commands/autoseen.js
+++ modules/commands/autoseen.js
@@ -24,21 +24,11 @@ module.exports. run = async ({ api, event, args }) => {
   try {
 	if (args[0] == 'on') {
 	  fs.writeFileSync(pathFile, 'true');
-	  api.sendMessage({body:`[🐧] - 𝗗𝗮̃ 𝗯𝗮̣̂𝘁 𝗰𝗵𝗲̂́ 𝗱𝗼̣̂ 𝘁𝘂̛̣ 𝗱𝗼̣̂𝗻𝗴 𝘀𝗲𝗲𝗻 𝗸𝗵𝗶 𝗰𝗼́ 𝘁𝗶𝗻 𝗻𝗵𝗮̆́𝗻 𝗺𝗼̛́𝗶`, attachment: (await global.nodemodule["axios"]({
-url: (await global.nodemodule["axios"]('https://docs-api.catteam123.repl.co/images/girl?apikey=JRTfree_1827425146')).data.data,
-method: "GET",
-responseType: "stream"
-})).data
-},event.threadID,event.messageID);
+	  api.sendMessage('Đã bật chế độ tự động seen khi có tin nhắn mới', event.threadID, event.messageID);
 	}
 	else if (args[0] == 'off') {
 	  fs.writeFileSync(pathFile, 'false');
-	  api.sendMessage({body:`[🐔] - 𝗗𝗮̃ 𝘁𝗮̆́𝘁 𝗰𝗵𝗲̂́ 𝗱𝗼̣̂ 𝘁𝘂̛̣ 𝗱𝗼̣̂𝗻𝗴 𝘀𝗲𝗲𝗻 𝗸𝗵𝗶 𝗰𝗼́ 𝘁𝗶𝗻 𝗻𝗵𝗮̆́𝗻 𝗺𝗼̛́𝗶`, attachment: (await global.nodemodule["axios"]({
-url: (await global.nodemodule["axios"]('https://docs-api.catteam123.repl.co/nsfw/ig?apikey=JRTfree_1827425146')).data.data,
-method: "GET",
-responseType: "stream"
-})).data
-},event.threadID,event.messageID);
+	  api.sendMessage('Đã tắt chế độ tự động seen khi có tin nhắn mới', event.threadID, event.messageID);
 	}
 	else {
 	  api.sendMessage('Sai cú pháp', event.threadID, event.messageID);
