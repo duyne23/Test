6894d7215df209dd462630f71fbb7b46e697839c
TNTxTrick <139092950+TNTxTrick@users.noreply.github.com>
2024-01-10 17:10:07 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,38 @@
-const { spawn } = require("child_process");
-const axios = require("axios");
-const logger = require("./utils/log");
+const http = require("http");
+const path = require('path');
 const express = require('express');
+const logger = require("./utils/log");
+const port = process.env.PORT || 8080;
+const moment = require("moment-timezone");
+const { spawn } = require("child_process");
+const { readFileSync } = require("fs-extra");
+var gio = moment.tz("Asia/Ho_Chi_Minh").format("HH:mm:ss");
 const app = express();
-const port = process.env.PORT || 2007;
-app.listen(port);
+
+
 
 app.get('/', function(req, res) {
-res.sendFile(path.join(__dirname, '/index.html'));
- });
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
 app.listen(port);
+console.log('𝑀𝑎́𝑦 𝑐ℎ𝑢̉ 𝑏𝑎̆́𝑡 đ𝑎̂̀𝑢 𝑡𝑎̣𝑖 http://localhost:' + port,"𝑉𝑎̀𝑜 𝑙𝑢́𝑐:" + gio,"\n\n");
 
 function startBot(message) {
     (message) ? logger(message, "BOT STARTING") : "";
-
     const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "marisv5.js"], {
-        cwd: __dirname,
-        stdio: "inherit",
-        shell: true
+        cwd: __dirname,stdio: "inherit",shell: true
     });
-
    child.on("close",async (codeExit) => {
       var x = 'codeExit'.replace('codeExit',codeExit);
         if (codeExit == 1) return startBot("BOT RESTARTING!!!");
          else if (x.indexOf(2) == 0) {
            await new Promise(resolve => setTimeout(resolve, parseInt(x.replace(2,'')) * 1000));
-                 startBot("Bot has been activated please wait a moment!!!");
+                 startBot("Bot đã hoạt động , vui lòng chờ trong giây lát!");
        }
          else return; 
     });
-
     child.on("error", function (error) {
-        logger("An error occurred: " + JSON.stringify(error), "[ Starting ]");
+        logger("Đã xảy ra lỗi: " + JSON.stringify(error), "[ Starting ]");
     });
 };
-
-axios.get("https://raw.githubusercontent.com/tandung1/Bot12/main/package.json").then((res) => {})
 startBot()
-
