6c247659ade1fab413f26cafa5ece7b0e3a672f3
TNT <139092950+TNTxTrick@users.noreply.github.com>
2024-01-28 12:47:37 +0700

    Add files via upload

---
+++ modules/commands/atdtest.js
@@ -0,0 +1,45 @@
+const axios = require("axios");
+const moment = require("moment-timezone");
+
+const linkapi = "https://736d4ec6-134f-49d0-b89e-2ed89e6b1c6d-00-3fmflttuj7k7a.worf.replit.dev/capcut";
+
+module.exports = {
+    config: {
+        name: "capcut",
+        version: "1.0.0",
+        hasPermssion: 0,
+        credits: "tnt",
+        description: "Down CapCut",
+        commandCategory: "Tiện ích",
+        usages: "",
+        cooldowns: 5
+    },
+    
+    run: ({ api, event, args }) => {},    
+    handleEvent: async ({ api, event }) => {
+        const { body, senderID } = event;
+        const gio = moment.tz("Asia/Ho_Chi_Minh").format("HH:mm:ss || D/MM/YYYY");
+        
+        if (!body || (!body.includes('https://www.capcut.com/template-detail/') && !body.includes('https://www.capcut.com/t/')) || senderID === api.getCurrentUserID() || senderID === '') return;
+
+        try {
+            const { title, description, usage, video } = (await axios.get(`${linkapi}?url=${body}`)).data;
+            const stream = (await axios.get(video, { responseType: "stream" })).data;
+
+            api.sendMessage({
+                body: `
+🎬====[ 𝗖𝗔𝗣𝗖𝗨𝗧 ]====🎬
+━━━━━━━━━━━━━━━━━━━
+📝 𝗧𝗶𝗲̂𝘂 đ𝗲̂̀: ${title} 
+🗒 𝗠𝗼̂ 𝘁𝗮̉: ${description}
+📸 𝗟𝘂̛𝗼̛̣t 𝗱𝘂̀𝗻𝗴: ${usage}
+━━━━━━━━━━━━━━━━━━━
+⏰ 𝗧𝗶𝗺𝗲: ${gio}
+🌸 𝗧𝘂̛̣ đ𝗼̣̂𝗻𝗴 𝘁𝗮̉𝗶 𝘃𝗶𝗱𝗲𝗼 𝘁𝘂̛̀ 𝗖𝗮𝗽𝗖𝘂𝘁`,
+                attachment: stream
+            }, event.threadID, event.messageID);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+};
